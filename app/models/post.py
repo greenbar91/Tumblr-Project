8d3a1091ee6b95ca5009ad8783a4aa95be835995
Jacob Dietz <jdietz981@gmail.com>
2024-05-29 21:47:11 -0500

    Commenting out tables for debugging

--- app/models/post.py
+++ app/models/post.py
@@ -5,7 +5,7 @@ class Post(db.Model):
     __tablename__ = "posts"
 
     if environment == "production":
-            __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
