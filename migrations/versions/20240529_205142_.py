77f76c15fcc17c5a7c46a449159280811f4aa2ee
Jacob Dietz <jdietz981@gmail.com>
2024-05-29 21:13:01 -0500

    Added postgresql additions in migrations

--- migrations/versions/20240529_205142_.py
+++ migrations/versions/20240529_205142_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: 6ac999f1a8a8
-Revises: 
+Revises:
 Create Date: 2024-05-29 20:51:42.264872
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -48,6 +52,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
