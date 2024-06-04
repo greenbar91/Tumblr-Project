48fd4ce686f6e1c63475a19211e8f34458b8f9db
Jacob Dietz <jdietz981@gmail.com>
2024-06-03 19:44:54 -0500

    Added more dummy seeds for testing

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -2,9 +2,25 @@ from app.models import db, Comment, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_comments():
+    # User1 comments
+    movie_comment = Comment(user_id=1, post_id=5, body="I've been meaning to see it, good to know its worth seeing!")
+    tv_comment = Comment(user_id=1, post_id=6, body="I've seen a few episodes, digging it so far, hopefully it stays good throughout")
+
+    # User2 comments
     cat_comment = Comment(user_id=2, post_id=1,body="OMG, what a cute kitty!")
+    dog_comment = Comment(user_id=2, post_id=2, body="Adorable. I have a Dachshund mix that likes to sleep all day.")
+
+    # User3 comments
+    dinner_comment = Comment(user_id=3, post_id=3, body="Yum! What herbs do you like in your sauce?")
+    dessert_comment = Comment(user_id=3, post_id=4, body="Ooooh that sounds so good right now. Was it homemade or store bought?")
+
 
     db.session.add(cat_comment)
+    db.session.add(dog_comment)
+    db.session.add(dinner_comment)
+    db.session.add(dessert_comment)
+    db.session.add(movie_comment)
+    db.session.add(tv_comment)
     db.session.commit()
 
 def undo_comments():
--- app/seeds/likes.py
+++ app/seeds/likes.py
@@ -2,9 +2,24 @@ from app.models import db, Like, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_likes():
+    # User1 Likes
+    movie_post_like = Like(user_id=1, post_id=5)
+    tv_post_like = Like(user_id=1, post_id=6)
+
+    # User2 Likes
     cat_post_like = Like(user_id=2, post_id=1)
+    dog_post_like = Like(user_id=2, post_id=2)
+
+    # User3 Likes
+    dinner_post_like = Like(user_id=3, post_id=3)
+    dessert_post_like = Like(user_id=3, post_id=4)
 
     db.session.add(cat_post_like)
+    db.session.add(dog_post_like)
+    db.session.add(dinner_post_like)
+    db.session.add(dessert_post_like)
+    db.session.add(movie_post_like)
+    db.session.add(tv_post_like)
     db.session.commit()
 
 def undo_likes():
