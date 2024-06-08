ee3e7560275cead8d697fecef1c826817950e188
Jacob Dietz <jdietz981@gmail.com>
2024-06-08 14:44:00 -0500

    Pushing to test main

---
+++ react-vite/src/components/CommentsPage/CommentsPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import "./CommentsPage.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { getCommentsByPostIdThunk } from "../../redux/comment";
+
+function CommentsPage({ postId }) {
+  const dispatch = useDispatch();
+  const comments = useSelector((store) => store.comments.comments.comments);
+  console.log(comments)
+
+  useEffect(() => {
+    dispatch(getCommentsByPostIdThunk(postId));
+  }, [dispatch, postId]);
+
+  return (
+    <div className="comments-container">
+
+
+          <div key={comments?.id}>
+            <div>{comments?.id}</div>
+            <div>{comments?.body}</div>
+
+          </div>
+
+
+    </div>
+  );
+}
+
+export default CommentsPage;
--- react-vite/src/components/Explore/Explore.jsx
+++ react-vite/src/components/Explore/Explore.jsx
@@ -1,18 +1,22 @@
 import "./Explore.css";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { FaRegComment } from "react-icons/fa";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postLikeThunk, deleteLikeThunk } from "../../redux/like";
 import { fetchAllPostsThunk } from "../../redux/post";
+import CommentsPage from "../CommentsPage";
+import { getCommentsByPostIdThunk } from "../../redux/comment";
 
 const Explore = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.postState.allPosts);
   const userLikes = useSelector((state) => state.likes.likes);
+  const [selectedPostId, setSelectedPostId] = useState(null);
 
   useEffect(() => {
     dispatch(fetchAllPostsThunk());
+    // dispatch(getCommentsByPostIdThunk());
   }, [dispatch, userLikes]);
 
   const handleLike = async (postId) => {
@@ -21,6 +25,17 @@ const Explore = () => {
       await dispatch(deleteLikeThunk(postId));
     } else {
       await dispatch(postLikeThunk(postId));
+    }
+  };
+
+  const handleCommentClick = async (postId) => {
+    if (selectedPostId === postId) {
+      // If the same post is clicked again, clear the selectedPostId
+      setSelectedPostId(null);
+    } else {
+      setSelectedPostId(postId);
+      // Dispatch action to fetch comments by postId
+      await dispatch(getCommentsByPostIdThunk(postId));
     }
   };
 
@@ -37,7 +52,11 @@ const Explore = () => {
               <p>{post.body}</p>
               <div className="post-stats">
                 <span>
-                  <FaRegComment /> {post.comment_count}
+                  <FaRegComment
+                    onClick={() => handleCommentClick(post.id)}
+                    className={selectedPostId === post.id ? "active" : ""}
+                  />
+                  {post.comment_count}
                 </span>
                 <span onClick={() => handleLike(post.id)}>
                   {hasLiked ? (
@@ -48,6 +67,7 @@ const Explore = () => {
                   {post.like_count}
                 </span>
               </div>
+              <CommentsPage postId={post.id} />
             </li>
           );
         })}
