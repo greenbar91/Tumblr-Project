a8acc22998dc9e4b6ca1653f788be9b9724cd463
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-06-08 19:38:17 -0700

    Add createNewPost thunk

--- react-vite/src/redux/post.js
+++ react-vite/src/redux/post.js
@@ -1,11 +1,19 @@
 import { csrfFetch } from './csrf';
 
 const LOAD_ALL_POSTS = 'post/LOAD_ALL_POSTS';
+const ADD_POST = 'post/ADD_POST';
 
 const loadAllPosts = (posts) => {
     return {
         type: LOAD_ALL_POSTS,
         posts
+    };
+};
+
+const addPost = (post) => {
+    return {
+        type: ADD_POST,
+        post
     };
 };
 
@@ -15,6 +23,22 @@ export const fetchAllPosts = () => async (dispatch) => {
     if (response.ok) {
         const data = await response.json();
         dispatch(loadAllPosts(data.Posts));
+    }
+};
+
+export const createNewPost = (formData) => async (dispatch) => {
+    const response = await csrfFetch(`api/posts`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+        const newPost = await response.json();
+        dispatch(addPost(newPost));
+        return newPost;
     }
 };
 
@@ -28,6 +52,11 @@ const postReducer = (state = initialState, action) => {
             return {
                 ...state,
                 allPosts: action.posts
+            };
+        case ADD_POST:
+            return {
+                ...state,
+                allPosts: [...state.allPosts, action.post]
             };
         default:
             return state;
