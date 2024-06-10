4bd75ef20714daf2fc0e55c5d50b3c67528d69ea
Jacob Dietz <jdietz981@gmail.com>
2024-06-10 14:56:42 -0500

    Finished createComment component

--- react-vite/src/components/PostComment/PostComment.jsx
+++ react-vite/src/components/PostComment/PostComment.jsx
@@ -1,46 +1,67 @@
 import "./PostComment.css";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCommentsByPostIdThunk, postCommentByPostIdThunk } from "../../redux/comment";
+import {
+  getCommentsByPostIdThunk,
+  postCommentByPostIdThunk,
+} from "../../redux/comment";
 import { fetchPostByIdThunk } from "../../redux/post";
+import { PiArrowLineRightBold } from "react-icons/pi";
 
-function PostComment({postId}) {
+function PostComment({ postId }) {
   const dispatch = useDispatch();
-  const comments = useSelector(
-    (store) => store.comments.comments_by_id?.comments || []
-  );
+  //   const comments = useSelector(
+  //     (store) => store.comments.comments_by_id?.comments || []
+  //   );
   const currentUser = useSelector((store) => store.session.user);
   const [commentText, setCommentText] = useState("");
   const [errors, setErrors] = useState({});
 
-  useEffect(()=> {
-    dispatch(fetchPostByIdThunk(postId))
-  },[dispatch, postId])
-
+  useEffect(() => {
+    dispatch(fetchPostByIdThunk(postId));
+  }, [dispatch, postId]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrors({})
+    setErrors({});
 
     const newComment = {
-        user_id: Number(currentUser.id),
-        post_id: postId,
-        body: commentText
+      user_id: Number(currentUser.id),
+      post_id: postId,
+      body: commentText,
+    };
+
+    const data = await dispatch(postCommentByPostIdThunk(postId, newComment));
+
+    if (!data?.message) {
+      dispatch(getCommentsByPostIdThunk(postId));
+      dispatch(fetchPostByIdThunk(postId));
+      setCommentText("");
     }
 
-    const data = await dispatch(postCommentByPostIdThunk(postId, newComment))
-
-    if(!data?.message){
-        dispatch(getCommentsByPostIdThunk(postId))
-        dispatch(fetchPostByIdThunk(postId))
+    if (data?.message) {
+      setErrors(data);
     }
-  }
+  };
 
   return (
     <div className="comment-container">
       <div className="post-comment-container">
         <form className="comment-form" onSubmit={handleSubmit}>
-
+          {errors && <p>{errors.message}</p>}
+          <textarea
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            className="comment-textarea"
+            placeholder={`reply as @${currentUser.username}`}
+          />
+          <button
+            type="submit"
+            className="submit-comment"
+            disabled={commentText.length < 1}
+          >
+            <PiArrowLineRightBold />
+          </button>
         </form>
       </div>
     </div>
