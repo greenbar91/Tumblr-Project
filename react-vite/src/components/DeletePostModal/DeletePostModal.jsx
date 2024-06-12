4c3abd7d9de0b13fe07bcc2c67b5582e75c4bfce
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-06-12 15:42:29 -0700

    Partially style the DeletePostModal

--- react-vite/src/components/DeletePostModal/DeletePostModal.jsx
+++ react-vite/src/components/DeletePostModal/DeletePostModal.jsx
@@ -16,8 +16,8 @@ const DeletePostModal = ({ postId }) => {
     return (
         <div className="delete-post-modal">
             <h1>Are you sure you want to delete this post?</h1>
-            <button className="keep-post" onClick={closeModal}>Cancel</button>
-            <button className="delete-post" onClick={handleDelete}>OK</button>
+            <button id="keep-post" onClick={closeModal}>Cancel</button>
+            <button id="delete-post" onClick={handleDelete}>OK</button>
         </div>
     );
 };
