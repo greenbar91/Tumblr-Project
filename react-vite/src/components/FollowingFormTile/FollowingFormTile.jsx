db9d79be454025e6cf7cac802673b44a96151b72
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-06-12 20:01:10 -0700

    Added user profile pic to follows"

--- react-vite/src/components/FollowingFormTile/FollowingFormTile.jsx
+++ react-vite/src/components/FollowingFormTile/FollowingFormTile.jsx
@@ -10,18 +10,19 @@ function FollowingFormTile({
     updated,
 }){
     const dispatch = useDispatch()
+console.log(icon)
     // const followingList = useSelector(state => state.follow.following)
 
     const handleUnfollow = async (e) => {
         e.preventDefault()
 
-        await dispatch(unfollowUserThunk(id))
+        await dispatch(unfollowUserThunk(id)).then(data => console.log(data))
     }
 
     return(
         <div className="following_tile">
             <div>
-                <img src={icon} alt="icon"></img>
+                <img src={icon} alt="icon" width={50} height={50}></img>
             </div>
             <div id='user_updated'>
                 <label >{username}</label>
