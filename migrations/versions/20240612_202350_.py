89460da17c999e072970f67ef00abaeace71c906
Jacob Dietz <jdietz981@gmail.com>
2024-06-13 19:07:54 -0500

    Added prod ruleset in migration

--- migrations/versions/20240612_202350_.py
+++ migrations/versions/20240612_202350_.py
@@ -8,6 +8,10 @@ Create Date: 2024-06-12 20:23:50.888327
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'a3f664ced221'
@@ -64,6 +68,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
