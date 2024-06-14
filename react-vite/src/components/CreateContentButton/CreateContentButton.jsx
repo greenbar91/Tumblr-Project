bf6d7d3ed9e1c6b407cc88e6093e1d0cda5c4c2f
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-06-13 22:24:33 -0700

    Flip the position of the 'Text circle' and 'Create button' of the CreateContentButton component

--- react-vite/src/components/CreateContentButton/CreateContentButton.jsx
+++ react-vite/src/components/CreateContentButton/CreateContentButton.jsx
@@ -1,7 +1,7 @@
 import './CreateContentButton.css';
 import { FaPencil } from "react-icons/fa6";
 import { IoText } from "react-icons/io5";
-import { FaCameraRetro } from "react-icons/fa";
+// import { FaCameraRetro } from "react-icons/fa";
 import { useState, useEffect, useRef } from 'react';
 import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 import CreatePostFormModal from '../CreatePostFormModal';
@@ -32,14 +32,14 @@ const CreateContentButton = () => {
     }, [showButtons]);
 
 
-    const buttons = [
-        { type: 'Photo', icon: <FaCameraRetro /> },
-        // { type: 'Quote', icon: '💬' },
-        // { type: 'Link', icon: '🔗' },
-        // { type: 'Chat', icon: '💬' },
-        // { type: 'Audio', icon: '🎧' },
-        // { type: 'Video', icon: '🎥' }
-    ];
+    // const buttons = [
+    //     { type: 'Photo', icon: <FaCameraRetro /> },
+    //     { type: 'Quote', icon: '💬' },
+    //     { type: 'Link', icon: '🔗' },
+    //     { type: 'Chat', icon: '💬' },
+    //     { type: 'Audio', icon: '🎧' },
+    //     { type: 'Video', icon: '🎥' }
+    // ];
 
     return (
         <div className="create-content-button-container">
@@ -58,14 +58,14 @@ const CreateContentButton = () => {
                         <span className="button-text">Text</span>
                     </div>
 
-                    {buttons.map((button, index) => (
+                    {/* {buttons.map((button, index) => (
                         <div key={index} className="content-button">
                             <div className={`circle ${button.colorClass}`}>
                                 {button.icon}
                             </div>
                             <span className="button-text">{button.type}</span>
                         </div>
-                    ))}
+                    ))} */}
                 </div>
             )}
         </div>
